8fd8453ce48e88f7e3bb2adbad57bd456c1064c8
Jeff <jeffjunio.bt@gmail.com>
2024-08-28 16:30:06 -0300

    first commit

---
+++ src/main/java/jeff/dev/RegisterAddressCliente/RegisterAddressClienteApplication.java
@@ -0,0 +1,13 @@
+package jeff.dev.RegisterAddressCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterAddressClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegisterAddressClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jeff/dev/RegisterAddressCliente/RegisterAddressClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package jeff.dev.RegisterAddressCliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegisterAddressClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
